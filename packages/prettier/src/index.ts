7396780e75884ba240fed03725d9eb2b5b5ab1ea
Shane McGuirt <10144278+srmcguirt@users.noreply.github.com>
2022-11-18 21:49:22 -0500

    added prettier config
    module and unit test built and checked

---
+++ packages/prettier/src/index.ts
@@ -0,0 +1,3 @@
+import prettierConfig = require('./lib/prettier')
+
+export { prettierConfig }
---
+++ packages/prettier/src/lib/prettier.ts
@@ -0,0 +1,9 @@
+import { Config } from 'prettier'
+
+export = {
+  printWidth: 100,
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true
+} as Config
